0c4fd3be7aa05c467700ac87f108f7afca593330
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2024-11-05 21:02:54 +0530

    Implemented the app-layout

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-9 text-center bg-gray-800 mt-10">Made by Piyush</div>
+    </div>
+  )
+};
+
+export default AppLayout;
