f38669e93a3bacab82624944700cd5f858cb293d
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2024-12-10 20:52:43 +0530

    Added filters for searching jobs

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,7 +1,20 @@
+import { getCompanies } from "@/api/apiCompanies";
 import { getJobs } from "@/api/apiJobs";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import JobCard from "@/components/ui/job-card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import useFetch from "@/hooks/useFetch";
 import { useSession, useUser } from "@clerk/clerk-react";
+import { State } from "country-state-city";
 import React, { useEffect, useState } from "react";
 import { BarLoader } from "react-spinners";
 
@@ -21,7 +34,13 @@ const JobListing = () => {
     searchQuery,
   });
 
+  const { fn: fnCompanies, data: companies } = useFetch(getCompanies);
+
   console.log(jobs);
+
+  useEffect(() => {
+    if (isLoaded) fnCompanies();
+  }, [isLoaded]);
 
   useEffect(() => {
     if (isLoaded) fnJobs();
@@ -31,6 +50,20 @@ const JobListing = () => {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7d7" />;
   }
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+
+    const query = formData.get("search-query");
+    if (query) setSearchQuery(query);
+  };
+
+  const clearFilters = () => {
+    setSearchQuery("");
+    setCompany_id("");
+    setLocation("");
+  };
+
   return (
     <div>
       <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
@@ -38,16 +71,87 @@ const JobListing = () => {
       </h1>
 
       {/* add filters */}
+      <form
+        onSubmit={handleSearch}
+        className="h-14 flex w-full gap-2 items-center mb-3"
+      >
+        <Input
+          type="text"
+          placeholder="Search Jobs by Title..."
+          name="search-query"
+          className="h-full flex-1 px-4 text-md"
+        />
+        <Button type="submit" className="h-full sm:w-28" variant="blue">
+          Search
+        </Button>
+      </form>
+
+      <div className="flex flex-col sm:flex-row gap-2">
+        <Select value={location} onValueChange={(value) => setLocation(value)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Location" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {State.getStatesOfCountry("IN").map(({ name }) => {
+                return (
+                  <SelectItem value={name} key={name}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select
+          value={company_id}
+          onValueChange={(value) => setCompany_id(value)}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Company" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {companies?.map(({ name, id }) => {
+                return (
+                  <SelectItem value={id} key={name}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Button
+          variant="destructive"
+          className="sm:w-1/2"
+          onClick={clearFilters}
+        >
+          Clear Filters
+        </Button>
+      </div>
 
       {loadingJobs && (
         <BarLoader className="mt-4" width={"100%"} color="#36d7d7" />
       )}
 
       {loadingJobs === false && (
-        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4" width={"100%"} color="#36d7b7">
+        <div
+          className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4"
+          width={"100%"}
+          color="#36d7b7"
+        >
           {jobs?.length ? (
             jobs.map((job) => {
-              return <JobCard key={job.id} job={job} />
+              return (
+                <JobCard
+                  key={job.id}
+                  job={job}
+                  savedInit={job?.saved?.length > 0}
+                />
+              );
             })
           ) : (
             <div>No Jobs Found 😢</div>
