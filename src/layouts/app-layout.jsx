6d364a0d31e7418a792030a40f6ac4c4dbec45e3
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2024-12-11 17:29:08 +0530

    fixed the layout css issue

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,18 +1,17 @@
 import Header from "@/components/Header";
-import React from "react";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <div className="grid-background"></div>
-      <main className="min-h-screen container">
+      <main className="min-h-screen mx-auto p-4 sm:px-6 lg:px-8 max-w-7xl">
         <Header />
         <Outlet />
       </main>
-      <div className="p-9 text-center bg-gray-800 mt-10">Made by Piyush</div>
+      <div className="p-10 text-center bg-gray-800 mt-10">Made by Piyush</div>
     </div>
-  )
+  );
 };
 
 export default AppLayout;
