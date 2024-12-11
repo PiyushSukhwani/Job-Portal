26927605f5e5f9cb15a6558e0ab2384600e82afc
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2024-12-11 18:41:18 +0530

    Implemented the UI of job page and the api to fetch the single job

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -32,31 +32,46 @@ export async function getJobs(token, { location, company_id, searchQuery }) {
 export async function saveJob(token, { alreadySaved }, saveData) {
   const supabase = await supabaseClient(token);
 
- if (alreadySaved) {
-  const {data, error: deleteError} = await supabase
-  .from("saved_jobs")
-  .delete()
-  .eq("job_id", saveData.job_id);
+  if (alreadySaved) {
+    const { data, error: deleteError } = await supabase
+      .from("saved_jobs")
+      .delete()
+      .eq("job_id", saveData.job_id);
 
-  if (deleteError) {
-    console.error("Error Deleting Saved Job: ", deleteError);
-    return null;
+    if (deleteError) {
+      console.error("Error Deleting Saved Job: ", deleteError);
+      return null;
+    }
+
+    return data;
+  } else {
+    const { data, error: insertError } = await supabase
+      .from("saved_jobs")
+      .insert([saveData])
+      .select();
+
+    if (insertError) {
+      console.error("Error fetching Jobs:", insertError);
+      return null;
+    }
+
+    return data;
   }
-
-  return data;
- } else {
-  const {data, error: insertError} = await supabase
-  .from("saved_jobs")
-  .insert([saveData])
-  .select()
-
-  if (insertError) {
-    console.error("Error fetching Jobs:", insertError);
-    return null;
-  }
-
-  return data;
 }
 
+export async function getSingleJob(token, { job_id }) {
+  const supabase = await supabaseClient(token);
 
+  const { data, error } = await supabase
+    .from("jobs")
+    .select("*, company:companies(name, logo_url), applications: applications(*)")
+    .eq("id", job_id)
+    .single();
+
+  if (error) {
+    console.error("Error fetching company: ", error);
+    return null;
+  }
+
+  return data;
 }
